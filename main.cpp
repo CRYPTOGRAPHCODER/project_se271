bccfdc9c99786b0a514aa78a387939a7e8a1ffa4
Lee <coder@dgist.ac.kr>
2017-11-15 10:14:31 +0900

    add player functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include "subject.h"
 #include "player.h"
-int main(int argc, char[][]args){
 
+void menu_main();
+
+int main(int argc, char[][]args){
+  menu_main();
   return 0;
 
+}
+
+void menu_main() {
+	boolean isKey_vaild = true;
+	while (isKey_vaild) {
+		system("cls");
+		printf("main menu\ns.start l.load x.exit \n >> ");
+		char menu_select = getchar();
+		while (getchar() != '\n');
+
+		switch (menu_select) {
+		case 'S':
+		case 's':
+			menu_start();
+			break;
+		case 'L':
+		case 'l':
+			//menu_load();
+			break;
+		case 'X':
+		case 'x':
+			isKey_vaild = false;
+			break;
+		default:
+			break;
+		}
+	}
 }
--- player.h
+++ player.h
@@ -2,16 +2,19 @@
 #define __PLAYER_H__
 
 #include <cstring>
+constexpr int STATS = 5;
+constexpr int EQUIPMENTS_MAX = 10;
 
 class player(){
 private:
   std::string name;
-  int life;
-  int money;
   int stats;
   int equipments;
-  int credit_acquired;
+  int life;
+  int money;
+  int credit_acquired_ess;
   int credit_required_ess;
+  int credit_acquired_chs;
   int credit_required_chs;
   int score;
 public:
