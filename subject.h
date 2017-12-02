754d3027ba99e4dc39bd492b6751c4b8096d498b
R3C0D3r <cryptographcode@gmail.com>
2017-12-02 16:25:19 +0900

    Update subject.h

--- subject.h
+++ subject.h
@@ -14,7 +14,6 @@ private:
   int attend_hope;
   int attend_people;
   int category;
-  int score;
 public:
   subject();
   ~subject();
@@ -23,17 +22,14 @@ public:
   // title
   std::string get_title() { return this->title; };
   void set_title(std::string value) { this->title = value; };
-  void add_title(std::string value) { this->title += value; };
 
   //timetale
   int* get_timetable() { return this->timetable; };
   void set_timetable(int value, int i) { timetable[i] = value; };
-  void add_timetable(int value, int i) { timetable[i] += value; };
 
   //credit
   int get_credit() { return this->credit; };
   void set_credit(int value) { this->credit = value; };
-  void add_credit(int value) { this->credit += value; };
 
   //attend_limit
   int get_attend_limit() { return this->attend_limit; };
@@ -53,17 +49,10 @@ public:
   //category
   int get_category() { return this->category; };
   void set_category(int value) { this->category = value; };
-  void add_category(int value) { this->category += value; };
-
-  //score
-  int get_score() { return this->score; };
-  void set_score(int value) { this->score = value; };
-  void add_score(int value) { this->score += value; };
 
   //workload
   double get_workload() { return this->score; };
   void set_workload(double value) { this->score = value; };
-  void add_workload(double value) { this->score += value; };
 };
 
 #endif
